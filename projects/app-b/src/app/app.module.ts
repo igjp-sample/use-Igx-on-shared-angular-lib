655c4b630e55155e52b2b132debc5c789b7d53ce
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-09-26 11:28:03 +0900

    ng add igniteui-angular を実行し、Ignite UI for Angular をワークスペースに組み込み

--- projects/app-b/src/app/app.module.ts
+++ projects/app-b/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LibXModule } from 'lib-x';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    LibXModule
+    LibXModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
